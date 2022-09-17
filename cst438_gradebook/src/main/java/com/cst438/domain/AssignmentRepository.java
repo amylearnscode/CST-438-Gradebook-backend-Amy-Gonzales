6af06d760abd077aea7cc50f8ab14403e19f04ba
Amy Gonzales <41878493+amylearnscode@users.noreply.github.com>
2022-09-17 16:04:52 -0700

    Committing DEV branch

--- cst438_gradebook/src/main/java/com/cst438/domain/AssignmentRepository.java
+++ cst438_gradebook/src/main/java/com/cst438/domain/AssignmentRepository.java
@@ -2,6 +2,7 @@ package com.cst438.domain;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -10,4 +11,13 @@ public interface AssignmentRepository extends CrudRepository <Assignment, Intege
 
 	@Query("select a from Assignment a where a.needsGrading=1 and a.dueDate < current_date and a.course.instructor= :email order by a.id")
 	List<Assignment> findNeedGradingByEmail(@Param("email") String email);
+	
+	@Modifying
+	@Query("INSERT INTO assignment (due_date, name, course_id, needs_grading) VALUES(:dueDate, :assignmentName, :courseId, 1)")
+	void newAssignment(@Param("assignmentName") String assignmentName, @Param("dueDate") String dueDate, @Param("courseId") int courseId);
+   
+	
+	  @Modifying
+	   @Query("UPDATE assignment a set a.name = :assignmentName where a.id = :assignmentId")
+	   void updateAssignment(@Param("assignmentName") String assignmentName, @Param("assignmentId") int assignmentId);
 }
